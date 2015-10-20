0f67478c8e101577276187f5e9c9a781b8021bdf
kvasanth <evervasanth@gmail.com>
2015-10-20 18:55:58 +0530

    testing ubunt

--- test.py
+++ test.py
@@ -2,9 +2,9 @@ import os
 import sys
 
 if sys.platform == 'linux32':
-	print "lin32"
+	print "ubunt excepted was 32"
 	print "testing the linux expected"
 else:
-	print "windows"
+	print "windows8 was excepted , so changed"
 	print "Not Excepted"
 
